88d471841157b6096b4b6445933b6eea0a474190
ArtZ <snickgem@gmail.com>
2021-03-25 19:09:52 +0400

    build

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/* libs */
+
+//= libs/jquery-3.3.1.min.js
+//= libs/jquery.fancybox.min.js
+//= libs/jquery.maskedinput.min.js
+//= libs/slick.min.js
+//= libs/jquery.mCustomScrollbar.min.js
+
+/* my scripts */
+
+//= partials/main.js
+//= partials/main-2.js
