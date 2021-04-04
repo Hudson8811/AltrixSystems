8459a258075cd18928801dcef1ddfdfacef6ccce
Hudson8811 <46067142+Hudson8811@users.noreply.github.com>
2021-04-04 19:04:06 +0300

    Главная десктоп

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 /* libs */
 
 //= libs/jquery-3.3.1.min.js
-//= libs/jquery.fancybox.min.js
-//= libs/jquery.maskedinput.min.js
-//= libs/slick.min.js
-//= libs/jquery.mCustomScrollbar.min.js
+// libs/jquery.fancybox.min.js
+// libs/jquery.maskedinput.min.js
+// libs/slick.min.js
+// libs/jquery.mCustomScrollbar.min.js
+//= libs/swiper-bundle.min.js
 
 /* my scripts */
 
-//= partials/main.js
+// partials/main.js
 //= partials/main-2.js
--- src/js/partials/main-2.js
+++ src/js/partials/main-2.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
+  const projectsCarousel = new Swiper('.__js_projects-carousel', {
+		slidesPerView: 'auto',
+		spaceBetween: 58,
+		loop: true,
+		pagination: {
+			el: '.paginate',
+			clickable: true
+		},
+		navigation: {
+			nextEl: '.nav-btn--next',
+			prevEl: '.nav-btn--prev',
+		}
+  });
 
+  const mainSlider = new Swiper('.__js_main-slider', {
+    slidesPerView: 1,
+		spaceBetween: 0,
+		loop: true,
+		pagination: {
+			el: '.paginate',
+			clickable: true
+		},
+  })
 });
