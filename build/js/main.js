f38357708245214284608c3ee78aa0c417015f0d
Hudson8811 <46067142+Hudson8811@users.noreply.github.com>
2021-04-05 19:06:55 +0300

    коммит

--- build/js/main.js
+++ build/js/main.js
@@ -47,5 +47,19 @@ $(document).ready(function(){
 			el: '.paginate',
 			clickable: true
 		},
+  });
+
+  const burger = $('.burger');
+  burger.on('click', function() {
+    $(this).toggleClass('is-active');
+  });
+
+  const footerMenuToggle = $('.footer__menu-title');
+  
+  footerMenuToggle.on('click', function() {
+    if ($(window).width() <= 580) {
+      $(this).toggleClass('is-active').next().slideToggle(300);
+    }
   })
+  
 });
